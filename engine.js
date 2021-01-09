a5144513155d963d43bba5acffc2d6cbb1f2cbde
OldAgario <36843941+OldAgario@users.noreply.github.com>
2021-01-09 10:56:11 +0300

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,12 +1,10 @@
-window.core_js = new Hack();
-
 var engine = new XMLHttpRequest;
 
 engine.open('GET', document.getElementById('enginejs').getAttribute('path'), false);
 
 engine.onload = function () {
     var script = engine.responseText;
-		script = script.replace(/\bLeaderboard/, 'if (0)'); //ZOOM
+		script = script.replace(/\bLeaderboard/, 'gogogo'); //ZOOM
     eval(script);
 };
 engine.send();
